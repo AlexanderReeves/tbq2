1c376c3d4f1ad4b2ba75396bd23decc680a42678
reevesdrums <reevesdrums@gmail.com>
2023-11-15 17:17:20 +1100

    nothing

--- behavior_packs/tbq2/scripts/Main.js
+++ behavior_packs/tbq2/scripts/Main.js
@@ -2,20 +2,18 @@
 // Spamming the chat with "Hello World"
 
 // Import world component from "@minecraft/server"
-import { world, system } from '@minecraft/server';
+import * from '@minecraft/server';
 
+import * as ui from '@minecraft/server-ui'
 // Subscribe to an event that calls every Minecraft tick
-//system.runInterval(() => {
+system.runInterval(() => {
 // Spams the chat with "Hello World" with world.sendMessage function in API
 //world.sendMessage("Hello World");
 
 // or run a command in overworld dimension
 // using native API methods (such as world.sendMessage) are recommended whenever possible.
-//world.getDimension("overworld").runCommand("say Hello World");
-//});
-
-system.runInterval(() => {
-	// Spams the chat with "Hello World" with world.sendMessage function in API
-	//world.sendMessage("running");
-    
+world.getDimension("overworld").runCommand("say Hello World");
 });
+
+
+
